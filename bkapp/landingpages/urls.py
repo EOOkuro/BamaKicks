5472bbe0cca7c1618ee0a6c880907b1c3916a964
EOOkuro <ezraokuro@gmail.com>
2019-02-04 23:01:35 -0800

    update urls file in the landingpages app

---
+++ bkapp/landingpages/urls.py
@@ -0,0 +1,10 @@
+urlpatterns = [
+    path('home/', home.site.urls),
+    path('about/', about.site.urls),
+    path('projects/', projects.site.urls),
+    path('contact/', contact.site.urls),
+    path('careers/', careers.stie.urls),
+    path('investor-relations/', investor-relations.site.urls),
+	path('terms-of-service/', terms-of-service.site.urls),
+    path('privacy-policy/', privacy.policy.site.urls),
+]
