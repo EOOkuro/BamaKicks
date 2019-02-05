a9ffd93a0ebc15be5a8ded350af4482e396bab4d
EOOkuro <ezraokuro@gmail.com>
2019-02-04 22:55:58 -0800

    change the footer and header, add pp,tos, careers, partners, and investor pages; add all urls to page.

--- bkapp/bkapp/urls.py
+++ bkapp/bkapp/urls.py
@@ -18,4 +18,12 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home.site.urls),
+    path('about/', about.site.urls),
+    path('projects/', projects.site.urls),
+    path('contact/', contact.site.urls),
+    path('careers/', careers.stie.urls),
+    path('investor-relations/', investor-relations.site.urls),
+	path('terms-of-service/', terms-of-service.site.urls),
+    path('privacy-policy/', privacy.policy.site.urls),
 ]
